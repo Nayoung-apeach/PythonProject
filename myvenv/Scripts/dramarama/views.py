c99ef4752a878897633026319a2c2ae992526ae0
minjeong <mjkimdelta@gmail.com>
2020-11-27 16:38:38 +0900

    Before starting connect form

--- myvenv/Scripts/dramarama/views.py
+++ myvenv/Scripts/dramarama/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("It's dramarama!")
+    context = {}
+    template_name = 'dramarama/index.html'
+    return render(request, template_name)
 
