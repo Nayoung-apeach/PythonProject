b4d59d2e1b3ef1607bb6eb419cd936fbcd4963d0
minjeong <mjkimdelta@gmail.com>
2020-11-30 21:23:44 +0900

    누락된 result.html 추가

--- myvenv/Scripts/dramarama/models.py
+++ myvenv/Scripts/dramarama/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class drama(models.Model):
+    id = models.IntegerField(default=0, primary_key=True)
     title = models.CharField(max_length=200)
     channel = models.CharField(max_length=100)
     info_url = models.TextField()
